604bc086368b411e6cb98b5b000e090e7bb4b870
Eric Yang <xsw26845@gmail>
2022-07-11 11:49:51 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,15 @@
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue #繼續
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
+     
+#使用者輸入
+products = [] 
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -12,9 +23,10 @@ while True:
     products.append(p)
 #   products.append(p) 可以直接簡寫為 [name, price]就不用p了
 print(products)
+#印出購買紀錄
 for p in products:
 	print(p[0], '的價格是 ', p[1], '元')
-
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
